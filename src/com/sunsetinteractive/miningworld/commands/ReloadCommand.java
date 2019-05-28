a5bdf715df3d7b1238a7b872cff0535502e879a9
Evan <justgamerimportant@gmail.com>
2019-05-27 19:44:58 -0700

    Reload command setup

---
+++ src/com/sunsetinteractive/miningworld/commands/ReloadCommand.java
@@ -0,0 +1,11 @@
+package com.sunsetinteractive.miningworld.commands;
+
+import com.sunsetinteractive.miningworld.MiningWorld;
+
+public class ReloadCommand {
+	private final MiningWorld plugin;
+
+	public ReloadCommand(MiningWorld plugin) {
+		this.plugin = plugin;
+	}
+}
