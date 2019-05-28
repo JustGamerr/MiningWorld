b53c3ef5a8e85aff5521abe71214ab480397976b
Evan <justgamerimportant@gmail.com>
2019-05-27 19:44:54 -0700

    Main command setup

---
+++ src/com/sunsetinteractive/miningworld/commands/MiningWorldCommand.java
@@ -0,0 +1,28 @@
+package com.sunsetinteractive.miningworld.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.sunsetinteractive.miningworld.Messages;
+import com.sunsetinteractive.miningworld.MiningWorld;
+
+public class MiningWorldCommand implements CommandExecutor {
+
+	private final MiningWorld plugin;
+
+	public MiningWorldCommand(MiningWorld plugin) {
+		this.plugin = plugin;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if(player.hasPermission(Messages.menu_permission)) {
+			
+		}
+		return false;
+	}
+
+}
