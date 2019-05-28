00f657302d54504ea7642794397442bfef6794f0
Evan <justgamerimportant@gmail.com>
2019-05-27 19:45:07 -0700

    Upgrades command setup

---
+++ src/com/sunsetinteractive/miningworld/commands/UpgradesCommand.java
@@ -0,0 +1,11 @@
+package com.sunsetinteractive.miningworld.commands;
+
+import com.sunsetinteractive.miningworld.MiningWorld;
+
+public class UpgradesCommand {
+	private final MiningWorld plugin;
+
+	public UpgradesCommand(MiningWorld plugin) {
+		this.plugin = plugin;
+	}
+}
