bcfabe8d14aecf7874a835ab6abc849b888ff2ae
Evan <justgamerimportant@gmail.com>
2019-05-27 19:45:13 -0700

    Version command setup

---
+++ src/com/sunsetinteractive/miningworld/commands/VersionCommand.java
@@ -0,0 +1,11 @@
+package com.sunsetinteractive.miningworld.commands;
+
+import com.sunsetinteractive.miningworld.MiningWorld;
+
+public class VersionCommand {
+	private final MiningWorld plugin;
+
+	public VersionCommand(MiningWorld plugin) {
+		this.plugin = plugin;
+	}
+}
