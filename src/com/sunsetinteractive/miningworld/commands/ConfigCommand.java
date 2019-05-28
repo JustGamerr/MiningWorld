c9ed7acf0f9da669104d134af15f173ab46716ef
Evan <justgamerimportant@gmail.com>
2019-05-27 19:44:43 -0700

    Configuration command setup

---
+++ src/com/sunsetinteractive/miningworld/commands/ConfigCommand.java
@@ -0,0 +1,13 @@
+package com.sunsetinteractive.miningworld.commands;
+
+import com.sunsetinteractive.miningworld.MiningWorld;
+
+public class ConfigCommand {
+
+	private final MiningWorld plugin;
+
+	public ConfigCommand(MiningWorld plugin) {
+		this.plugin = plugin;
+	}
+	
+}
