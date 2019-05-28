b1f34eef33fec9da38c8a2581d630163522f083f
Evan <justgamerimportant@gmail.com>
2019-05-27 20:03:11 -0700

    MiningWorld Messages reference location fix

--- src/com/sunsetinteractive/miningworld/commands/MiningWorldCommand.java
+++ src/com/sunsetinteractive/miningworld/commands/MiningWorldCommand.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.sunsetinteractive.miningworld.Messages;
+import com.sunsetinteractive.miningworld.utils.Messages;
 import com.sunsetinteractive.miningworld.MiningWorld;
 
 public class MiningWorldCommand implements CommandExecutor {
