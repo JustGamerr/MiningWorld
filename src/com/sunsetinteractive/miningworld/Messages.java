1354a39d0f383bc4552b213bf573511d99b5305d
Evan <justgamerimportant@gmail.com>
2019-05-27 19:46:47 -0700

    Messages initalisation class

---
+++ src/com/sunsetinteractive/miningworld/Messages.java
@@ -0,0 +1,15 @@
+package com.sunsetinteractive.miningworld;
+
+public class Messages
+{
+	public static String prefix;
+	public static String plugin_reloaded;
+	public static String missing_permission;
+	public static String menu_permission;
+	public static String use_mine_permsision;
+	public static String view_stats_permission;
+	public static String upgrades_permission;
+	public static String admin_permission;
+	public static String config_permission;
+	public static String reload_permission;
+}
