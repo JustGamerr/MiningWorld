3347a12e6a69e7987fb68bc6577f389a6cccc78f
Evan <justgamerimportant@gmail.com>
2019-05-27 19:45:02 -0700

    Stats command setup

---
+++ src/com/sunsetinteractive/miningworld/commands/StatsCommand.java
@@ -0,0 +1,11 @@
+package com.sunsetinteractive.miningworld.commands;
+
+import com.sunsetinteractive.miningworld.MiningWorld;
+
+public class StatsCommand {
+	private final MiningWorld plugin;
+
+	public StatsCommand(MiningWorld plugin) {
+		this.plugin = plugin;
+	}
+}
