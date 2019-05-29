1856080bc92c46c32020959d65ccfeb24995bccb
Evan <justgamerimportant@gmail.com>
2019-05-28 23:51:26 -0700

    Configurable messages class setup w/ new messages & perms

---
+++ src/com/sunsetinteractive/miningworld/utils/Configuration.java
@@ -0,0 +1,19 @@
+package com.sunsetinteractive.miningworld.utils;
+
+public class Configuration
+{
+	public static String prefix;
+	public static String plugin_reloaded;
+	public static String missing_permission;
+	public static String menu_permission;
+	public static String use_mine_permsision;
+	public static String view_stats_permission;
+	public static String upgrades_permission;
+	public static String admin_permission;
+	public static String config_permission;
+	public static String reload_permission;
+	public static String view_others_stats_permission;
+	public static String others_upgrades_permission;
+	public static String manage_upgrades_permission;
+	
+}
