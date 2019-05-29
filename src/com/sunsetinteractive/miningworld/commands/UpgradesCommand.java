878a144dafd1351b70cf9275e42cd8a685df9ae2
Evan <justgamerimportant@gmail.com>
2019-05-28 23:49:49 -0700

    Commands setup package

--- src/com/sunsetinteractive/miningworld/commands/UpgradesCommand.java
+++ src/com/sunsetinteractive/miningworld/commands/UpgradesCommand.java
@@ -1,11 +1,27 @@
 package com.sunsetinteractive.miningworld.commands;
 
-import com.sunsetinteractive.miningworld.MiningWorld;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public class UpgradesCommand {
+import com.sunsetinteractive.miningworld.MiningWorld;
+import com.sunsetinteractive.miningworld.menus.Upgrades;
+import com.sunsetinteractive.miningworld.utils.Configuration;
+
+public class UpgradesCommand implements CommandExecutor {
 	private final MiningWorld plugin;
 
 	public UpgradesCommand(MiningWorld plugin) {
 		this.plugin = plugin;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if(player.hasPermission(Configuration.upgrades_permission)) {
+			new Upgrades(plugin, player);
+		}
+		return false;
 	}
 }
--- src/com/sunsetinteractive/miningworld/commands/VersionCommand.java
+++ src/com/sunsetinteractive/miningworld/commands/VersionCommand.java
@@ -1,11 +1,21 @@
 package com.sunsetinteractive.miningworld.commands;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 import com.sunsetinteractive.miningworld.MiningWorld;
 
-public class VersionCommand {
+public class VersionCommand implements CommandExecutor {
 	private final MiningWorld plugin;
 
 	public VersionCommand(MiningWorld plugin) {
 		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
